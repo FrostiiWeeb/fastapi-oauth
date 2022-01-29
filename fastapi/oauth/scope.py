be13ea4d93556c9e669e0399ddbc8325552012c3
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-01-29 11:52:22 +0100

    Fix some break/return stuff

--- fastapi/oauth/scope.py
+++ fastapi/oauth/scope.py
@@ -4,10 +4,11 @@ class Scope(object):
 	def __init__(self, scopes : List[str]) -> None:
 		self._scopes = "%20".join(scopes)
 		self.scope = self._scopes
+		self.__scopes = scopes
 
 	@property
 	def scopes(self):
-		return self._scopes
+		return " ".join(self.__scopes)
 
 	def __repr__(self) -> str:
 		return "<Scope {}>".format(" ".join([self.scopes]))
